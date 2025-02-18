a4e1a5539bad02761f1d3e01aa8f5c4f664ff0ef
s1h100 <siennahemmett@gmail.com>
2025-02-18 16:01:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+console.log("JavaScript is loaded!"); // Debugging test
+
 function toggleMenu() {
+    console.log("Menu button clicked!"); // Debugging test
     const menu = document.getElementById("nav-menu");
     menu.classList.toggle("hidden");
 }
